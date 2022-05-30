8077ecd57e9c5857ffe4dd81c476bc9f6b4c7c3c
mkokonyan <mkokonyan@gmail.com>
2022-05-30 12:06:38 +0300

    working with abstraction

--- src/L01_WorkingWithAbstraction/a_lab/P04_HotelReservation/Main.java
+++ src/L01_WorkingWithAbstraction/a_lab/P04_HotelReservation/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         String[] tokens = scanner.nextLine().split("\\s+");
+
         double pricePerDay = Double.parseDouble(tokens[0]);
         int numberOfDays = Integer.parseInt(tokens[1]);
         Season season = Season.valueOf(tokens[2]);
